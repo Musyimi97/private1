92d8038535cc76278ca6b80d90cfeecfa0569e68
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-24 10:56:04 +0300

    lgtm

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(*args,**kargs):
+    return  HttpResponse("<h1>jumboShop</h1>")
+
+
+def contact_view(*args,**kargs):
+    return  HttpResponse("<h1>Call Us Today to shop with us</h1>")
+
+def about_view(*args,**kargs):
+    return  HttpResponse("<h1>We are an online shop selling top range shoes and clothes for men</h1>")
+
+
+
--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
 
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args,**kargs):
-    return HttpResponse("<h1>jumboShop</h1>")
