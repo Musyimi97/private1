6b2c5488529a55f9c0ab87ac63ddac9666a02e5d
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-24 12:48:00 +0300

    templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, request
+
 
 # Create your views here.
-def home_view(*args,**kargs):
-    return  HttpResponse("<h1>jumboShop</h1>")
+def home_view(request):
+    print(request.user)
+    return render(request,"home.html",{})
 
 
-def contact_view(*args,**kargs):
-    return  HttpResponse("<h1>Call Us Today to shop with us</h1>")
+def contact_view(request,*args,**kargs):
+    return  render(request, "contact.html",{})
 
-def about_view(*args,**kargs):
-    return  HttpResponse("<h1>We are an online shop selling top range shoes and clothes for men</h1>")
+def about_view(request,*args,**kargs):
+    my_context={
+        "my_text":"This is Jumbo shop",
+        "my_list": [1313, 4218, 234, 'abc']
+
+
+    }
+    return  render(request, "about.html",my_context)
 
 
 
